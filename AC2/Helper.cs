85587e5618d8896f192475f2acb3a8f1720bfce7
sandroAntonucci <sandro.antonucci.7e7@itb.cat>
2024-04-07 21:14:28 +0200

    UPDATE

--- AC2/Helper.cs
+++ AC2/Helper.cs
@@ -43,7 +43,7 @@ namespace AC2
             return xmlData;
         }
 
-        // Identifica les comarques amb una població superior a 200000
+        // Identifica les comarques amb una població superior a maxPoblation
         public static List<ConsumAigua> XMLFindPopulationGreaterThan(int maxPoblation, string path)
         {
 
@@ -57,14 +57,7 @@ namespace AC2
 
         }
 
-        public static void PrintRegions(List<XElement> comarques)
-        {
-            foreach (var comarca in comarques)
-            {
-                Console.WriteLine("Hola");
-            }
-        }
-
+        // Converteix una llista XElement en ConsumAigua
         public static List<ConsumAigua> ConvertToClassConsum(List<XElement> comarques)
         {
             List<ConsumAigua> consums = new List<ConsumAigua>();
@@ -84,6 +77,111 @@ namespace AC2
                 consums.Add(consum);
             }
             return consums;
+        }
+
+        // Mostra per consola el consum mitjà per comarca
+        public static void CalcAvgConsumptionPerRegion(string path)
+        {
+            XDocument xmlData = XDocument.Load(path);
+
+            // Agrupa els consums per comarca i calcula la mitja de consum domèstic
+            var consumsPerComarca = xmlData.Root.Elements("Consum")
+                .GroupBy(e => (string)e.Element("Comarca"))
+                .Select(g => new
+                {
+                    Comarca = g.Key,
+                    ConsumMitja = g.Average(e => (float)e.Element("ConsDomPerCapita"))
+                })
+                .ToList();
+
+            // Mostra les dades per consola
+            foreach (var item in consumsPerComarca)
+            {
+                Console.WriteLine("Comarca: " + item.Comarca + " Consum mitjà: " + item.ConsumMitja);
+            }
+        }
+
+        // Retorna les 10 regions amb major consumició per càpita
+        public static List<ConsumAigua> RegionsWithHighestConsumption(string path)
+        {
+            XDocument xmlData = XDocument.Load(path);
+
+            var comarques = xmlData.Root.Elements("Consum")
+                .Select(e => new ConsumAigua
+                {
+                    Comarca = (string)e.Element("Comarca"),
+                    Poblacio = (int)e.Element("Poblacio"),
+                    Any = (int)e.Element("Any"),
+                    CodiComarca = (int)e.Element("CodiComarca"),
+                    DomXarxa = (int)e.Element("DomXarxa"),
+                    ActEcon = (int)e.Element("ActEcon"),
+                    Total = (int)e.Element("Total"),
+                    ConsDomPerCapita = (float)e.Element("ConsDomPerCapita")
+                })
+                .OrderByDescending(c => c.ConsDomPerCapita)
+                .Take(10)
+                .ToList();
+
+            return comarques;
+        }
+
+        // Retorna les 10 regions amb menor consumició per càpita
+        public static List<ConsumAigua> RegionsWithLowestConsumption(string path)
+        {
+            XDocument xmlData = XDocument.Load(path);
+
+            var comarques = xmlData.Root.Elements("Consum")
+                .Select(e => new ConsumAigua
+                {
+                    Comarca = (string)e.Element("Comarca"),
+                    Poblacio = (int)e.Element("Poblacio"),
+                    Any = (int)e.Element("Any"),
+                    CodiComarca = (int)e.Element("CodiComarca"),
+                    DomXarxa = (int)e.Element("DomXarxa"),
+                    ActEcon = (int)e.Element("ActEcon"),
+                    Total = (int)e.Element("Total"),
+                    ConsDomPerCapita = (float)e.Element("ConsDomPerCapita")
+                })
+                .OrderBy(c => c.ConsDomPerCapita)
+                .Take(10)
+                .ToList();
+
+            return comarques;
+        }
+
+        // Filtra les comarques per nom
+        public static List<ConsumAigua> FilterComarquesByName(string path, string nomComarca)
+        {
+            XDocument xmlData = XDocument.Load(path);
+            return FilterComarques(xmlData, e => (string)e.Element("Comarca") == nomComarca);
+        }
+
+        // Filtra les comarques per codi
+        public static List<ConsumAigua> FilterComarquesByCode(string path, int codiComarca)
+        {
+            XDocument xmlData = XDocument.Load(path);
+            return FilterComarques(xmlData, e => (int)e.Element("CodiComarca") == codiComarca);
+        }
+
+        // Filtra les comarques mitjançant un predicta
+        public static List<ConsumAigua> FilterComarques(XDocument xmlData, Func<XElement, bool> predicate)
+        {
+            var comarques = xmlData.Root.Elements("Consum")
+                .Where(predicate)
+                .Select(e => new ConsumAigua
+                {
+                    Any = (int)e.Element("Any"),
+                    CodiComarca = (int)e.Element("CodiComarca"),
+                    Comarca = (string)e.Element("Comarca"),
+                    Poblacio = (int)e.Element("Poblacio"),
+                    DomXarxa = (int)e.Element("DomXarxa"),
+                    ActEcon = (int)e.Element("ActEcon"),
+                    Total = (int)e.Element("Total"),
+                    ConsDomPerCapita = (float)e.Element("ConsDomPerCapita")
+                })
+                .ToList();
+
+            return comarques;
         }
 
     }
--- AC2/Program.cs
+++ AC2/Program.cs
@@ -1,49 +1,117 @@
-﻿using CsvHelper;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
 using System.Xml.Linq;
 
 namespace AC2
 {
-
     class Program
-    {
-
-        public static void Main()
+    {   
+        static void Main(string[] args)
         {
-
             const string CSVPath = "../../../Consum.csv";
             const string XMLPath = "../../../Consum.xml";
 
-            int length = 0;
+            const string MsgMenu = "\nMenú:\n1. Comarques amb població superior a 200,000\n" +
+                                          "2. Consum domèstic mitjà per comarca\n" +
+                                          "3. Comarques amb consum domèstic per càpita més alt\n" +
+                                          "4. Comarques amb consum domèstic per càpita més baix\n" +
+                                          "5. Filtrar comarques per codi\n" +
+                                          "6. Filtrar comarques per nom\n" +
+                                          "7. Sortir\n\nEscull una opció: ";
+            const string InvalidOptionMessage = "Opció no vàlida. Si us plau, selecciona una opció del menú.";
+            const string EnterCodeMessage = "Introdueix el codi de la comarca: ";
+            const string EnterNameMessage = "Introdueix el nom de la comarca: ";
+
+
+            const string MsgPoblationGreaterThan200000 = "Comarques amb població superior a 200,000:";
+            const string MsgMaxConsumption = "Comarques amb consum domèstic per càpita més alt:";
+            const string MsgMinConsumption = "Comarques amb consum domèstic per càpita més baix:";
+            const string MsgFilterByCode = "Comarques filtrades pel codi:";
+            const string MsgFilterByName = "Comarques filtrades pel nom:";
 
             const int MaxPoblation = 200000;
 
-            // Es converteixen les dades csv a objectes ConsumAigua
+            string action, name;
+
+            int code;
+
+            bool exit = false;
+
+
+            // Se convierten los datos CSV a objetos ConsumAigua
             List<ConsumAigua> consums = Helper.SaveCSV(CSVPath);
 
-            // Es guarden les dades en un arxiu xml
+            // Se guardan los datos en un archivo XML
             XElement xmlData = Helper.ConvertToXml(consums);
             xmlData.Save(XMLPath);
 
-            //Ex1
-            List<ConsumAigua> consumsMaxPoblation = Helper.XMLFindPopulationGreaterThan(MaxPoblation, XMLPath);
 
-            foreach(var consumAigua in consumsMaxPoblation)
+            while (!exit)
             {
-                length++;
-                Console.WriteLine(consumAigua);
+                Console.Write(MsgMenu);
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    // Població amb més de 200000
+                    case "1":
+                        List<ConsumAigua> consumsMaxPoblation = Helper.XMLFindPopulationGreaterThan(MaxPoblation, XMLPath);
+                        PrintConsums(MsgPoblationGreaterThan200000, consumsMaxPoblation);
+                        break;
+
+                    // Consum domèstic per càpita mitjà per comarca
+                    case "2":
+                        Helper.CalcAvgConsumptionPerRegion(XMLPath);
+                        break;
+
+                    // Consum domèstic per càpita més alt
+                    case "3":
+                        List<ConsumAigua> maxConsumsPerCapita = Helper.RegionsWithHighestConsumption(XMLPath);
+                        PrintConsums(MsgMaxConsumption, maxConsumsPerCapita);
+                        break;
+
+                    // Consum domèstic per càpita més baix
+                    case "4":
+                        List<ConsumAigua> minConsumsPerCapita = Helper.RegionsWithLowestConsumption(XMLPath);
+                        PrintConsums(MsgMinConsumption, minConsumsPerCapita);
+                        break;
+
+                    // Consums filtrats per codi
+                    case "5":
+                        Console.Write(EnterCodeMessage);
+                        code = Convert.ToInt32(Console.ReadLine());
+                        List<ConsumAigua> consumPerCodi = Helper.FilterComarquesByCode(XMLPath, code);
+                        PrintConsums(MsgFilterByCode, consumPerCodi);
+                        break;
+
+                    // Consums filtrats per nom
+                    case "6":
+                        Console.Write(EnterNameMessage);
+                        name = Console.ReadLine();
+                        List<ConsumAigua> consumPerNom = Helper.FilterComarquesByName(XMLPath, name);
+                        PrintConsums(MsgFilterByName, consumPerNom);
+                        break;
+
+                    // Sortir del menú
+                    case "7":
+                        exit = true;
+                        break;
+
+                    // Entrada no vàlida
+                    default:
+                        Console.WriteLine(InvalidOptionMessage);
+                        break;
+                }
             }
-            Console.WriteLine(length);
-
-
-
         }
 
+        static void PrintConsums(string message, List<ConsumAigua> consums)
+        {
+            Console.WriteLine(message);
+            foreach (var consum in consums)
+            {
+                Console.WriteLine(consum);
+            }
+        }
     }
-
-
 }
